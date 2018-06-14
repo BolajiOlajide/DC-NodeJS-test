5151a6e8456474af67b0fb2b72b539694db82ad7
Bolaji Olajide <bolaji.olajide@andela.com>
2018-06-14 16:15:33 +0100

    base setup for the API

---
+++ server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 7777;
+
+// Parse incoming requests data (https://github.com/expressjs/body-parser)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.set('port', process.env.PORT || 7777);
+
+// Setup a default catch-all route that sends back a welcome message in JSON format.
+app.get('*', (req, res) => res.status(200).send({
+  message: 'Welcome to the beginning of nothingness.',
+}));
+
+app.listen(port, err => {
+  err ? console.log(err) : console.log(`App is running on port ${port}`);
+});
